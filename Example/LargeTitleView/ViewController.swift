57a5068b328273fc4904c766c6e656a28570faa6
fuwenhua <shizhou@live.com>
2020-06-17 09:45:32 +0800

    add demo

--- Example/LargeTitleView/ViewController.swift
+++ Example/LargeTitleView/ViewController.swift
@@ -18,14 +18,37 @@ class ViewController: UIViewController {
             self.navigationItem.largeTitleDisplayMode = .automatic
         } else {
             // Fallback on earlier versions
-            let customLargeView = LargeTitleView
+            let customlargeView = LargeTitleView.init(title: "荒淫荒淫，热烈荒淫")
+            customlargeView.registerFatherView(backScrollView)
+            backScrollView.addSubview(customlargeView)
         }
+        view.addSubview(backScrollView)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "注册新用户", style: .plain, target: self, action: #selector(toRegiste))
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.isHidden = false
+        }else {
+            navigationController?.navigationBar.isHidden = true
+        }
+    }
+        
+    @objc func toRegiste() {
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    var backScrollView: UIScrollView = {
+        var scrollView = UIScrollView.init(frame: UIScreen.main.bounds)
+        scrollView.backgroundColor = UIColor.clear
+        scrollView.contentSize = CGSize.init(width: 0, height: UIScreen.main.bounds.size.height)
+        scrollView.showsVerticalScrollIndicator = false
+        return scrollView
+    }()
 }
 
